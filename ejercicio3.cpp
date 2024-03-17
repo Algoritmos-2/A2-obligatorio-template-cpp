7bb98169a77eba585bd49004b825b06822d86a01
Joaquin Vigna <joaquin@brokenrubik.co>
2024-03-17 09:39:31 -0300

    first commit

---
+++ ejercicio3.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    // TODO
+    return 0;
+}
---
+++ funciones/enteros.cpp
@@ -0,0 +1,16 @@
+
+int hacerPositivo(int i) {
+    if (i < 0)
+    {
+        return i * -1;
+    }
+    return i;
+}
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
